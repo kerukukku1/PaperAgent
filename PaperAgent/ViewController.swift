188003e6f314f0103e15d3013ecdbaa06d5a6db9
Mahito Fujino <kerukukku1@gmail.com>
2018-04-12 19:23:36 +0900

    Initial Commit

---
+++ PaperAgent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaperAgent
+//
+//  Created by 藤野眞人 on 2018/04/12.
+//  Copyright © 2018年 Mahito Fujino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
